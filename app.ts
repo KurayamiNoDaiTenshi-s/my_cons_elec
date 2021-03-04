4e8dcf09e7d43e852bff425cf4ee71608e1366a0
KurayamiNoDaiTenshi <angelo67170@gmail.com>
2021-03-04 14:00:45 +0000

    WIP

--- app.ts
+++ app.ts
@@ -1,12 +1,15 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import * as mongoose from 'mongoose';
+import {Sequelize} from 'sequelize';
 import * as path from 'path';
-import {router as stuffRoutes} from "./routes/stuff";
-import {router as userRoutes} from './routes/user';
-mongoose.connect('mongodb+srv://learn-expressjs:BORbY6H68uA8LvWb@tuto-express-mongodb.mzjeg.mongodb.net/test?retryWrites=true&w=majority')
-    .then(() => console.log('MongoDb connection success'))
-    .catch(() => console.log('MongoDb connection failed'));
+import * as dbConfig from './config/db.json';
+
+// import {router as stuffRoutes} from "./routes/stuff";
+// import {router as userRoutes} from './routes/user';
+let sequelize = new Sequelize(dbConfig["dev"])
+// mongoose.connect('mongodb+srv://learn-expressjs:BORbY6H68uA8LvWb@tuto-express-mongodb.mzjeg.mongodb.net/test?retryWrites=true&w=majority')
+//     .then(() => console.log('MongoDb connection success'))
+//     .catch(() => console.log('MongoDb connection failed'));
 const app = express();
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,6 +19,6 @@ app.use((req, res, next) => {
 });
 app.use(bodyParser.json());
 app.use('/images',express.static(path.join(__dirname, 'images')))
-app.use('/api/stuff',stuffRoutes)
-app.use('/api/auth',userRoutes)
+// app.use('/api/stuff',stuffRoutes)
+// app.use('/api/auth',userRoutes)
 export {app}
