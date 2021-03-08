09003ab8c5b6372b64694317df824876a80ad4b3
Angelo Basso <angelo67170@gmail.com>
2021-03-08 20:02:13 +0100

    WIP : change db config format and add usage of environment variable to switch config depending on dev machine

--- app.ts
+++ app.ts
@@ -1,12 +1,18 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import {Sequelize} from 'sequelize';
+import {Sequelize} from 'sequelize-typescript';
 import * as path from 'path';
 import * as dbConfig from './config/db.json';
 
 // import {router as stuffRoutes} from "./routes/stuff";
 // import {router as userRoutes} from './routes/user';
-let sequelize = new Sequelize(dbConfig["dev"])
+// let sequelize = new Sequelize(dbConfig["dev"])
+let devEnv: string = process.env.node_dev_env;
+if (devEnv === null) {
+    console.log('No specific dev environment configuration set/found switching to default');
+    devEnv = 'defaultDevEnv';
+}
+const sequelize = new Sequelize(dbConfig[devEnv]);
 // mongoose.connect('mongodb+srv://learn-expressjs:BORbY6H68uA8LvWb@tuto-express-mongodb.mzjeg.mongodb.net/test?retryWrites=true&w=majority')
 //     .then(() => console.log('MongoDb connection success'))
 //     .catch(() => console.log('MongoDb connection failed'));
@@ -18,7 +24,7 @@ app.use((req, res, next) => {
     next();
 });
 app.use(bodyParser.json());
-app.use('/images',express.static(path.join(__dirname, 'images')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
 // app.use('/api/stuff',stuffRoutes)
 // app.use('/api/auth',userRoutes)
 export {app}
