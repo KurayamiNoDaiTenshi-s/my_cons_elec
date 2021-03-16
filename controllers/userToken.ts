5daba1bd52ce98a8b0378e86a41fe9d5ec59fae9
Angelo Basso <angelo67170@gmail.com>
2021-03-16 21:55:31 +0100

    create userToken controller architecture

---
+++ controllers/userToken.ts
@@ -0,0 +1,19 @@
+export function generateToken() {
+
+}
+
+export function getToken(req, res) {
+
+}
+
+export function checkToken(req, res) {
+
+}
+
+export function deleteToken(req, res) {
+
+}
+
+export function setTokenUsed(req, res) {
+
+}
