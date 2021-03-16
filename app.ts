a108101a52658afc930d1d1c0622a6edca59eb2a
Angelo Basso <angelo67170@gmail.com>
2021-03-16 21:54:51 +0100

    add fetch method to get app parameters on app start

--- app.ts
+++ app.ts
@@ -2,9 +2,9 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import {Sequelize} from 'sequelize-typescript';
 import * as path from 'path';
+import * as _ from 'lodash';
 import * as dbConfig from './config/db.json';
-import {UserToken} from "./models/UserToken";
-import {User} from "./models/User";
+import {Parameter} from "./models/Parameter";
 
 // import {router as stuffRoutes} from "./routes/stuff";
 // import {router as userRoutes} from './routes/user';
@@ -14,9 +14,19 @@ if (devEnv === null) {
     console.log('No specific dev environment configuration set/found switching to default');
     devEnv = 'defaultDevEnv';
 }
+const app = express();
 const sequelize = new Sequelize(dbConfig[devEnv]);
 sequelize.addModels([__dirname + '/models/*.ts'])
-UserToken.findOne({where: {tokenString: 'tototiti'}}).then(token => {
+Parameter.findAll({mapToModel:true}).then(parameters=>{
+    const parameterMap = new Map();
+    _.forEach(parameters,parameter=>{
+        parameterMap.set(parameter.key,parameter.value)
+    })
+    app.set('parameters',parameterMap);
+}).catch(err=>{
+    console.log(`Error while trying to fetch the parameter table in the database : ${err}`)
+})
+/*UserToken.findOne({where: {tokenString: 'tototiti'}}).then(token => {
     if (token === null) {
         let token = new UserToken({tokenString: 'tototiti', createdBy: 'SYS_ADM'});
         token.save();
@@ -40,11 +50,10 @@ User.findOne({where: {email: 'angelo.basso.pro@gmail.com'}}).then(user => {
     } else {
         console.log('user exist skipping creation...')
     }
-})
+})*/
 // mongoose.connect('mongodb+srv://learn-expressjs:BORbY6H68uA8LvWb@tuto-express-mongodb.mzjeg.mongodb.net/test?retryWrites=true&w=majority')
 //     .then(() => console.log('MongoDb connection success'))
 //     .catch(() => console.log('MongoDb connection failed'));
-const app = express();
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -52,7 +61,7 @@ app.use((req, res, next) => {
     next();
 });
 app.use(bodyParser.json());
-app.use('/images', express.static(path.join(__dirname, 'images')))
+// app.use('/images', express.static(path.join(__dirname, 'images')))
 // app.use('/api/stuff',stuffRoutes)
 // app.use('/api/auth',userRoutes)
 export {app}
